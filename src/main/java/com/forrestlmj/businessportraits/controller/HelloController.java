7641f46170182d5f269077e8311e26db58866075
forrestlmj <18810276930@163.com>
2021-02-05 22:21:26 +0800

    项目初始化

---
+++ src/main/java/com/forrestlmj/businessportraits/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.forrestlmj.businessportraits.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello world";
+    }
+}
